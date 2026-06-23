098a3c1946fb799a222f7c13d829df7026187e62
Mahmoud Taha <blaxcq@gmail.com>
2026-06-24 03:11:55 +0530

    update

--- src/components/SkillList.jsx
+++ src/components/SkillList.jsx
@@ -1,4 +1,31 @@
+import { useState, useEffect } from 'react';
 export default function SkillList({ toggleSkillList }) {
+  const [pageNumber, setPageNumber] = useState(1);
+
+  useEffect(() => {
+    window.addEventListener('keydown', function (e) {
+      if (e.key === 'ArrowRight') {
+        return setPageNumber(2);
+      }
+    });
+  });
+
+  useEffect(() => {
+    window.addEventListener('keydown', function (e) {
+      if (e.key === 'ArrowLeft') {
+        return setPageNumber(1);
+      }
+    });
+  });
+
+  function previous() {
+    if (pageNumber > 1) setPageNumber((prev) => prev - 1);
+  }
+
+  function next() {
+    if (pageNumber < 2) setPageNumber((prev) => prev + 1);
+  }
+
   return (
     <main className="skillList" id="skillList">
       <div className="titleAndButton">
@@ -7,7 +34,127 @@ export default function SkillList({ toggleSkillList }) {
           Close
         </button>
       </div>
-      <div className="content"></div>
+      <div className="content">
+        {pageNumber < 2 ? (
+          <button className="previousDisabled" disabled>
+            &lt;
+          </button>
+        ) : (
+          <button className="previous" onClick={previous}>
+            &lt;
+          </button>
+        )}
+
+        {pageNumber < 2 ? (
+          <div className="titleAndFluencies">
+            <h4 className="titleOne">
+              Fluency&nbsp;<p>in</p>&nbsp;Cybersecurity
+            </h4>
+            <div className="fluencyOne">
+              <p className="name">WireShark</p>
+              <div className="statusBar">
+                <div className="bottom"></div>
+                <p className="percentage">70%</p>
+                <div className="topOne"></div>
+              </div>
+            </div>
+
+            {/* */}
+
+            <div className="fluencyTwo">
+              <p className="name">Nmap</p>
+              <div className="statusBar">
+                <div className="bottom"></div>
+                <p className="percentage">80%</p>
+                <div className="topTwo"></div>
+              </div>
+            </div>
+
+            {/* */}
+
+            <div className="fluencyThree">
+              <p className="name">Metasploit</p>
+              <div className="statusBar">
+                <div className="bottom"></div>
+                <p className="percentage">65%</p>
+                <div className="topThree"></div>
+              </div>
+            </div>
+
+            {/* */}
+
+            <div className="fluencyFour">
+              <p className="name">Burp Suite</p>
+              <div className="statusBar">
+                <div className="bottom"></div>
+                <p className="percentage">75%</p>
+                <div className="topFour"></div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="titleAndFluencies">
+            <h4 className="titleTwo">
+              Fluency&nbsp;<p>in</p>&nbsp;Front-End
+            </h4>
+            <div className="fluencyFive">
+              <p className="name">HTML</p>
+              <div className="statusBar">
+                <div className="bottom"></div>
+                <p className="percentage">100%</p>
+                <div className="topFive"></div>
+              </div>
+            </div>
+
+            {/* */}
+
+            <div className="fluencySix">
+              <span className="name">
+                CSS&nbsp;<p>&</p>&nbsp;SCSS
+              </span>
+              <div className="statusBar">
+                <div className="bottom"></div>
+                <p className="percentage">90%</p>
+                <div className="topSix"></div>
+              </div>
+            </div>
+
+            {/* */}
+
+            <div className="fluencySeven">
+              <p className="name">JavaScript</p>
+              <div className="statusBar">
+                <div className="bottom"></div>
+                <p className="percentage">70%</p>
+                <div className="topSeven"></div>
+              </div>
+            </div>
+
+            {/* */}
+
+            <div className="fluencyEight">
+              <p className="name">React</p>
+              <div className="statusBar">
+                <div className="bottom"></div>
+                <p className="percentage">80%</p>
+                <div className="topEight"></div>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {pageNumber > 1 ? (
+          <button className="nextDisabled" disabled>
+            &gt;
+          </button>
+        ) : (
+          <button className="next" onClick={next}>
+            &gt;
+          </button>
+        )}
+
+        <p>{pageNumber} / 2</p>
+      </div>
     </main>
   );
 }
