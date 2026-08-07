c95cc5b2a16f07077ff6d021befa67677073313d
Mahmoud Taha <blaxcq@gmail.com>
2026-08-08 00:41:36 +0530

    update

--- src/components/WebBox.jsx
+++ src/components/WebBox.jsx
@@ -1,4 +1,6 @@
+import { useState, useEffect, useRef, act } from 'react';
 import WebList from './WebList';
+
 export default function WebBox({
   kaffa,
   veila,
@@ -11,21 +13,130 @@ export default function WebBox({
   largeLandscape,
   largePortrait,
 }) {
+  const timerRef = useRef(null);
+  const containerRef = useRef(null);
+  const imageRefs = useRef([]);
+  //
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const carouselData = [
+    {
+      id: 1,
+      src: kaffa,
+      className: 'activeImg',
+      url: 'https://example.com',
+    },
+    {
+      id: 2,
+      src: veila,
+      className: 'activeImg',
+      url: 'https://example.com',
+    },
+    {
+      id: 3,
+      src: guessGame,
+      className: 'activeImg',
+      url: 'https://example.com',
+    },
+  ];
+  //
+  const currentItem = carouselData[currentIndex];
+  //
+  const isInitialLoad = useRef(true);
+  const [timeLeft, setTimeLeft] = useState(5);
+  const [isPaused, setIsPaused] = useState(false);
+  const [glow, setGlow] = useState(6);
+  //
+  const [nextImg, setNextImg] = useState(1);
+
+  // Initial delay only runs once on page start
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      isInitialLoad.current = false;
+    }, 7000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  // Related to loader animation delay and glow effect
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      timerRef.current = setInterval(() => {
+        setGlow((prev) => {
+          if (prev <= 0) {
+            clearInterval(timerRef.current);
+            return 0;
+          }
+          return prev - 1;
+        });
+      }, 1000);
+    });
+
+    return () => {
+      clearTimeout(timeoutId);
+      clearInterval(timerRef.current);
+    };
+  }, []);
+
+  // Related to web timer
+  useEffect(() => {
+    if (isPaused || isContactFormOpen || activeList) return;
+
+    const timerId = setInterval(() => {
+      if (isInitialLoad.current) return;
+
+      setTimeLeft((prev) => {
+        if (prev <= 1) {
+          setCurrentIndex((i) => (i + 1) % carouselData.length);
+          return 5;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(timerId);
+  }, [isPaused, isContactFormOpen, activeList, carouselData.length]);
+
+  // Carousel images auto scroll
+  useEffect(() => {
+    const container = containerRef.current;
+    const targetImage = imageRefs.current[currentIndex];
+
+    if (container && targetImage) {
+      const scrollPosition =
+        targetImage.offsetTop -
+        container.clientHeight / 2 +
+        targetImage.clientHeight / 2;
+
+      container.scrollTo({
+        top: scrollPosition,
+        behavior: 'smooth',
+      });
+    }
+  }, [nextImg]);
+
   return (
     <div className="webAndWebList">
       <div
+        key={timeLeft - { glow }}
         className="webBox"
-        style={
-          activeList || isContactFormOpen
+        style={{
+          ...(activeList || isContactFormOpen
             ? {
                 filter:
                   'opacity(0.5) grayscale(10%) blur(0.05rem) brightness(80%)',
               }
-            : null
-        }
+            : null),
+          animation:
+            activeList || isContactFormOpen
+              ? undefined
+              : glow
+                ? 'webBox 0.5s linear 1'
+                : !glow && timeLeft === 5 && 'webAlternate 1s ease 1',
+        }}
       >
         <p className="title">Web Dev. Projects</p>
         <div
+          ref={containerRef}
           className="carousel"
           style={
             activeList
@@ -35,31 +146,29 @@ export default function WebBox({
                 }
               : undefined
           }
+          onPointerEnter={() => setIsPaused(true)}
+          onPointerLeave={() => setIsPaused(false)}
         >
-          <a
-            href="https://example.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="firstImg"
-          >
-            <img src={kaffa} fetchPriority="high" />
-          </a>
-          <a
-            href="https://example.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="secondImg"
-          >
-            <img src={veila} fetchPriority="high" />
-          </a>
-          <a
-            href="https://example.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="thirdImg"
-          >
-            <img src={guessGame} fetchPriority="high" />
-          </a>
+          {carouselData.map((item, index) => (
+            <a
+              key={item.id}
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={
+                activeList || isContactFormOpen
+                  ? 'linkNonActive'
+                  : index === currentIndex
+                    ? 'linkActive'
+                    : 'linkNonActive'
+              }
+              ref={(el) => {
+                imageRefs.current[index] = el;
+              }}
+            >
+              <img src={item.src} fetchPriority="high" />
+            </a>
+          ))}
         </div>
         {activeList || isContactFormOpen ? (
           <button className="moreDisabled">More</button>
@@ -74,6 +183,42 @@ export default function WebBox({
             More
           </button>
         )}
+
+        <div
+          className="nextWebLoader"
+          style={{
+            ...(timeLeft === 5
+              ? { animation: 'nextWebLoader 1s linear 1' }
+              : undefined),
+          }}
+        >
+          <div
+            className="nextWebOval"
+            style={{
+              opacity: activeList || isContactFormOpen ? 0 : 1,
+              ...(glow
+                ? { animation: 'nextWebStops 1s linear 1' }
+                : { animation: 'nextWebOval 1.5s linear infinite' }),
+              ...(!activeList && !isContactFormOpen && glow
+                ? { animation: 'nextWebGlow 3s linear infinite' }
+                : undefined),
+            }}
+          ></div>
+
+          {!glow && (
+            <div
+              className="nextWebTimer"
+              style={{
+                opacity: activeList || isContactFormOpen ? 0 : 1,
+                ...(!activeList && !isContactFormOpen && timeLeft
+                  ? { animation: 'nextWebTimer 1s linear 1' }
+                  : undefined),
+              }}
+            >
+              {timeLeft}
+            </div>
+          )}
+        </div>
       </div>
       {largeLandscape || largePortrait
         ? undefined
