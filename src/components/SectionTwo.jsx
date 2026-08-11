45bcd0884a46eb917385300cf31b28d120285a1b
Mahmoud Taha <blaxcq@gmail.com>
2026-08-11 20:22:05 +0530

    update

--- src/components/SectionTwo.jsx
+++ src/components/SectionTwo.jsx
@@ -63,6 +63,8 @@ export default function SectionTwo({
       >
         <section className="projects">
           <CyberBox
+            pauseTwo={pauseTwo}
+            playTwo={playTwo}
             isContactFormOpen={isContactFormOpen}
             activeList={activeList}
             toggleCyberList={toggleCyberList}
