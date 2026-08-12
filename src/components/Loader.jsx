9d9729c9a91266fdb39f4d60bfe6ea71a4af4c50
Mahmoud Taha <blaxcq@gmail.com>
2026-08-12 10:03:58 +0530

    update

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,18 +1,11 @@
 export default function Loader({ progress }) {
   return (
     <div className="loaderContainer">
-      {/*  */}
       <div className="loader">
-        {/*  */}
         <div className="spinnerBrown"></div>
-        {/*  */}
         <p className="percentage">{progress}%</p>
-        {/*  */}
         <div className="spinnerOlive"></div>
       </div>
-      {/*  */}
-      <p className="disclaimer">Loading Components</p>
-      {/*  */}
     </div>
   );
 }
