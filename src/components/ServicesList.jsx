d9ad56a47d0f214ba03033be03a2c6ef36962813
Mahmoud Taha <blaxcq@gmail.com>
2026-09-05 02:15:40 +0530

    update

--- src/components/ServicesList.jsx
+++ src/components/ServicesList.jsx
@@ -81,7 +81,7 @@ export default function ServicesList({
       },
     });
 
-    const imgData = canvas.toDataURL('image/jpeg', 1.5);
+    const imgData = canvas.toDataURL('image/jpeg', 2);
 
     const testImage = document.createElement('img');
 
